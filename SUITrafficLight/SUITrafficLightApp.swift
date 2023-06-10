09c6d8cae36f031a376b97589f7a684b955ca4a3
Danila Kornev <danilakornev92@gmail.com>
2023-06-10 21:29:31 +0300

    Initial Commit

---
+++ SUITrafficLight/SUITrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUITrafficLightApp.swift
+//  SUITrafficLight
+//
+//  Created by Danila Kornev on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUITrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
